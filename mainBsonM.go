ed37576896423abf1733bcf76ed474abad3eea38
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-07-13 20:38:36 +0530

    BSON M

--- mainBsonM.go
+++ mainBsonM.go
@@ -1,2 +1,24 @@
 // bson.M
 // bson.M is a map representation of a BSON document. It's defined as:
+
+type M map[string]interface{}
+
+package main
+
+import (
+    "fmt"
+    "go.mongodb.org/mongo-driver/bson"
+)
+
+func main() {
+
+	doc := bson.M{
+		"name": "John Doe",
+		"age":30,
+		"address":bson.M{
+			"street": "23",
+            "city":   "India",
+		}
+	}
+	fmt.Println(doc)
+}
